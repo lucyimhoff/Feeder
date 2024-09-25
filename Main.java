087c8f0372b5d556260c91e29ce750d919eba488
lucyimhoff <lucyi4@nycstudents.net>
2024-09-25 09:33:01 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,16 +3,29 @@ public class Main
     public static void main(String[] args) 
     {
         
-        //Feeder f = new Feeder();
-        //System.out.println(f);
-       //System.out.println(f.currentFood);
-       //System.out.println(f.getCurrentFood());
-       //Feeder g = new Feeder(500);
-       //System.out.println(g.getCurrentFood());
-       //int random = (int) (Math.random() * 50 + 1);
-       //System.out.println(random);
-        Feeder j = new Feeder(250);
-        System.out.println(j.simulateManyDays(10, 5));
+        Feeder f = new Feeder(500);
+        f.simulateOneDay(12);
+        System.out.println(f);
+
+        Feeder h = new Feeder(1000);
+        h.simulateOneDay(22);
+        System.out.println(h);
+
+        Feeder a = new Feeder(100);
+        a.simulateOneDay(5);
+        System.out.println(a);
+
+        Feeder b = new Feeder(2400);
+        System.out.println(b.simulateManyDays(10, 4));
+
+        Feeder c = new Feeder(250);
+        System.out.println(c.simulateManyDays(10,5));
+
+        Feeder d = new Feeder(0);
+        System.out.println(d.simulateManyDays(5, 10));
+
+
+
     }
 
 }
